22352e0d7369a6d78cf8a7585030d0fe7c7bc5ed
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-13 03:00:50 -0300

    refactor: update routes folder

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./views/Home";
+import Login from "./views/Login";
 import Register from "./views/Register";
 
 export default function myRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
